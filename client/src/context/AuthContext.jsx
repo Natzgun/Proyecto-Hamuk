6dd32d78c1b89145d171e03e393536e87cda3b38
Erick <itzlilouxx@gmail.com>
2023-08-05 23:34:10 -0500

    Solving vulnerabilities

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -21,7 +21,7 @@ export const AuthProvider = ({ children }) => {
   const signup = async (user) => {
     try {
       const res = await registerRequest(user);
-      console.log(res.data);
+      //console.log(res.data);
       setUser(res.data);
       setIsAuthenticated(true);
     } catch (err) {
@@ -33,7 +33,7 @@ export const AuthProvider = ({ children }) => {
   const signin = async (user) => {
     try {
       const res = await loginRequest(user);
-      console.log(res.data);
+      //console.log(res.data);
       setIsAuthenticated(true);
       setUser(res.data);
     } catch (error) {
