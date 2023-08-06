1015d5e5210b3358cbb200c8a2d0ece40583b4a8
Erick <itzlilouxx@gmail.com>
2023-08-06 10:13:53 -0500

    Solving bug Cookies

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -60,7 +60,7 @@ export const login = async (request, response) => {
 
     const token = await createAccessToken({ id: userFound._id });
     response.cookie('token', token, {
-      httpOnly: process.env.NODE_ENV !== "development",
+      httpOnly: true,
       secure: true,
       sameSite: "none",
       domain: process.env.NODE_ENV === 'development' ? 'localhost' : 'hamuk-api.vercel.app'
